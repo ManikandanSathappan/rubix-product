ed0b711d0f5467e728d334d6487836feb17196b3
Manikandan-Sathappan <110529419+Manikandan-Sathappan@users.noreply.github.com>
2023-04-05 10:15:18 +0530

    side nav bug fix

--- rubix-books/src/app/main/side-nav-bar/side-nav-bar.component.ts
+++ rubix-books/src/app/main/side-nav-bar/side-nav-bar.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-nav-bar.component.css']
 })
 export class SideNavBarComponent {
+/* Set the width of the side navigation to 250px */
+ openNav() {
+  (document.getElementById('mySidenav') as HTMLElement).style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+ closeNav() {
+  (document.getElementById("mySidenav") as HTMLElement).style.width = "0";
+}
+
+add(){
+  (document.querySelector("body") as HTMLElement).classList.toggle("active");
+}
 }
